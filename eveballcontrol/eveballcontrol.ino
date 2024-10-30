9874b2fa71070e58ea6a4a455a3eb59a83f0b80d
Clementine Allen <callen42@uw.edu>
2024-10-29 17:39:54 -0700

    updated with diagonal

--- eveballcontrol/eveballcontrol.ino
+++ eveballcontrol/eveballcontrol.ino
@@ -6,6 +6,7 @@ Adafruit_PWMServoDriver pca9685 = Adafruit_PWMServoDriver();
 
 // Servo channel assignments
 const int blink_channel = 0;    // Blink servo channel
+const int diagonal_channel = 1; // Diagonal movement servo channel
 const int vertical_channel = 2; // Vertical (Y) movement servo channel
 
 // Servo pulse range limits
@@ -13,50 +14,12 @@ const int blinkMin = 630;
 const int blinkMax = 450;
 const int verticalMin = 150;
 const int verticalMax = 300;
-
+const int diagonalMin = 400;
+const int diagonalMax = 500;
 // Variables to hold the current pulse positions of the servos
 int blink_currentPulse = blinkMax;    // Start with the eye open (blinkMax)
-int vertical_currentPulse = 225;      // Center position for vertical servo
-
-// Variables for blinking behavior
-unsigned long lastBlinkTime = 0;
-unsigned long blinkInterval = 5000;  // Random blink every ~5 seconds
-bool blink = false;
-
-// Variables for vertical movement behavior
-bool movingUp = true;
-
-void setup() {
-  Serial.begin(9600);
-
-  // Initialize PCA9685 controller and set frequency for servos
-  pca9685.begin();
-  pca9685.setPWMFreq(60);  // 60 Hz for servos
-
-  // Move servos to default (centered) positions
-  smoothMoveTo(blink_channel, blinkMax, blink_currentPulse); // Open the blink servo
-  smoothMoveTo(vertical_channel, vertical_currentPulse, vertical_currentPulse); // Center vertical servo
-}
-
-#include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
-
-// Create an instance of the PCA9685 controller
-Adafruit_PWMServoDriver pca9685 = Adafruit_PWMServoDriver();
-
-// Servo channel assignments
-const int blink_channel = 0;    // Blink servo channel
-const int vertical_channel = 2; // Vertical (Y) movement servo channel
-
-// Servo pulse range limits
-const int blinkMin = 630;
-const int blinkMax = 450;
-const int verticalMin = 150;
-const int verticalMax = 300;
-
-// Variables to hold the current pulse positions of the servos
-int blink_currentPulse = blinkMax;    // Start with the eye open (blinkMax)
-int vertical_currentPulse = 225;      // Center position for vertical servo
+int vertical_currentPulse = (verticalMax - verticalMin) / 2;      // Center position for vertical servo
+int diagonal_currentPulse = (diagonalMax - diagonalMin) / 2;     // Center position for diagonal servo
 
 // Variables for blinking behavior
 unsigned long lastBlinkTime = 0;
